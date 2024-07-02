ccaa2ea25b32f7b356a3c455f049a99c45c05b2b
TomixJasina <thomas.jasinski90@gmail.com>
2024-07-02 20:54:53 +0100

    Added global collections names constant
    Added global RavenDB query quick reference for collections
    
    Fixed wildcard query search
    
    Sorted variables in DataHandler
    
    General fixes

--- src/plugins/service-wispstock/views/betterportal/products/index.ts
+++ src/plugins/service-wispstock/views/betterportal/products/index.ts
@@ -4,20 +4,19 @@ import {
   BetterPortalUIComponentDataHandler,
   HTTP_DATA_METHOD,
 } from "@bettercorp/betterportal";
-import {z} from "zod";
-import {Plugin} from "../../../plugin";
-import {T_SUPPORTED_THEMES} from "../index";
+import { z } from "zod";
+import { Plugin } from "../../../plugin";
+import { T_SUPPORTED_THEMES } from "../index";
+import { ProductListItem, QueryCollections } from "../../../../../index";
 
 const querySchema = z.object({
-  page: z.coerce.number()
-         .min(1)
-         .optional(),
+  page: z.coerce.number().min(1).optional(),
   search: z.string().optional(),
 });
 const outputSchema = z.object({
   products: z.array(
     z.object({
-    title: z.string(),
+      title: z.string(),
       img: z.string(),
       sku: z.string(),
     })
@@ -28,9 +27,9 @@ const outputSchema = z.object({
 });
 
 export interface ViewDefinition
-    extends BetterPortalUIComponentBaseTypeDefinition {
-  context?: Plugin,
-  path: "/products",
+  extends BetterPortalUIComponentBaseTypeDefinition {
+  context?: Plugin;
+  path: "/products";
   query: typeof querySchema;
   inboundData: null;
   outputData: typeof outputSchema;
@@ -42,45 +41,41 @@ export const ViewSchema: ViewDefinition = {
   query: querySchema,
   inboundData: null,
   outputData: outputSchema,
-  methods: [
-    "GET",
-  ],
+  methods: ["GET"],
 };
 
-import {Component as Materio1} from "./materio1";
+import { Component as Materio1 } from "./materio1";
 
-export const ViewHandlers: Record<T_SUPPORTED_THEMES, BetterPortalUIView<ViewDefinition> | null> = {
+export const ViewHandlers: Record<
+  T_SUPPORTED_THEMES,
+  BetterPortalUIView<ViewDefinition> | null
+> = {
   materio1: Materio1,
 };
 
 export const DataHandler: BetterPortalUIComponentDataHandler<
   ViewDefinition
 > = async (props) => {
-  const session = props.context.RavenDB.openSession();
-
-  const page = props.query.page ?? 1;
-
-  const search = props.query.search;
+  const session = props.context.RavenDB.openSession(),
+    page = props.query?.page ?? 1;
 
   let query1 = session
-    .query<ProductListItem>({ collection: "products" })
-    .whereNotEquals("enabled", true);
+      .query<ProductListItem>(QueryCollections.products)
+      .whereEquals("enabled", true),
+    query2 = session
+      .query<ProductListItem>(QueryCollections.products)
+      .whereEquals("enabled", true);
 
-  let query2 = session
-    .query<ProductListItem>({ collection: "products" })
-    .whereNotEquals("enabled", true);
-
-  if (search) {
-    query1 = query1.whereEquals("title", search);
-    query2 = query2.whereEquals("title", search);
+  if (props.query?.search) {
+    query1 = query1.search("title",`*${props.query.search}*` ).orElse().search("sku", `*${props.query.search}*`);
+    query2 = query2.search("title",`*${props.query.search}*`).orElse().search("sku", `*${props.query.search}*`);
   }
 
-  const total = await query1.count();
-
-  const products = await query2
-    .skip(props.context.Config.maxProductsPerPage * (page - 1))
-    .take(props.context.Config.maxProductsPerPage)
-    .all();
+  const total = await query1.count(),
+    products = await query2
+      .skip(props.context.Config.maxProductsPerPage * (page - 1))
+      .take(props.context.Config.maxProductsPerPage)
+      .all();
 
   session.dispose();
 
@@ -94,11 +89,14 @@ export const DataHandler: BetterPortalUIComponentDataHandler<
       })),
       page: page,
       total: total,
-      perPage: props.context.Config.maxProductsPerPage,
+      perPage: props.context.Config.maxProductsPerPage
     },
   };
 };
-export const DataHandlers: Record<HTTP_DATA_METHOD, BetterPortalUIComponentDataHandler<ViewDefinition> | undefined> = {
+export const DataHandlers: Record<
+  HTTP_DATA_METHOD,
+  BetterPortalUIComponentDataHandler<ViewDefinition> | undefined
+> = {
   POST: DataHandler,
   GET: DataHandler,
   PATCH: undefined,
