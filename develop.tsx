bb4816bfa8fe0532300eff93d38c0680260f62b3
Mst Bilkis Naher <bilkis.naher@yahoo.co.uk>
2024-08-27 17:19:49 +0100

    added a head line

---
+++ develop.tsx
@@ -0,0 +1,16 @@
+<div>
+<p class="text-warning bg-drak"><h1>New Arrival</h1></p>
+</div>
+<div class="col-sm-6 bg-warning text-white p-3">
+              <a target="_blank" href="https://scoop.co.za/download/marketing/images/UVC-AITH.jpg">
+                <img src="https://scoop.co.za/download/marketing/images/UVC-AITH.jpg" alt="camera" width="700" height="500" /></a>
+              <div class=""> </div>
+            </div>
+
+          
+            <div class="col-sm-6 bg-danger text-white p-3">
+            <a target="_blank" href="https://media.stockinthechannel.com/pic/ihFEBSj9VUG_wL95ql7UeQ.c-t.jpg">
+                <img src="https://media.stockinthechannel.com/pic/ihFEBSj9VUG_wL95ql7UeQ.c-t.jpg" alt="camera" width="700" height="500" /></a>
+              <div class=""> </div>
+             
+            </div>
