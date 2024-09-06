f55e5f25527206029d311cafd6df3a1f6c608cb1
TomixJasina <thomas.jasinski90@gmail.com>
2024-09-06 21:07:17 +0100

    Deleted img of product Added styling

--- src/plugins/service-wispstock/views/betterportal/products/materio1.tsx
+++ src/plugins/service-wispstock/views/betterportal/products/materio1.tsx
@@ -15,20 +15,14 @@ export const Component: BetterPortalUIView<ViewDefinition> = (props) => {
   const productsList = products.map((product) => (
 
     <div class="card cardStyle border rounded-3 m-1 px-auto ">
-      <div class="card-body frame">
-         <img
-            src={props.context.methods.getProductImageUrl(product.id)}
-            class="img-fluid w-100"
-          ></img>
-      </div>
 
       <div class="card-body frame text-white text-center mt-1">
 
       <div class="frame text-white text-center m-1"> 
-          <h6 class="text-white text-center p-2">{product.title}</h6>
-        </div>
+          <h6 class="text-white text-center pt-4">{product.title}</h6>
+      </div>
       
-      <div class="d-flex justify-content-evenly mt-1">
+      <div class="d-flex justify-content-evenly pt-4">
         <p>
           <img src="https://cdn.shopify.com/s/files/1/1439/1668/t/28/assets/favicon.ico?v=69860357381422628591672729793" class="icon"></img>
         </p>
@@ -39,13 +33,13 @@ export const Component: BetterPortalUIView<ViewDefinition> = (props) => {
           <img src="https://www.ispsupplies.com/img/icon-192.png" class="icon"></img> 
         </p>
       </div>
-      <div class="d-flex justify-content-evenly">
+      <div class="d-flex justify-content-evenly pt-4">
         <i class="icon bi bi-heart"></i>
         <i class="icon bi bi-info-circle" hx-get={props.context.methods.getProductDetailsUrl(product.id)}></i>
       </div>
 
       </div>
-        <p class="card-text text-center">SKU: {product.sku}</p>
+        <p class="card-text text-center py-4">SKU: {product.sku}</p>
       </div>
 
   ));
